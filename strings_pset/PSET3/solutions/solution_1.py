0496976bf3a1dd9d206589e04dcdd4d0bda3b91b
Omprakash <om381640@gmail.com>
2022-12-28 16:15:21 +0530

    Add files

---
+++ strings_pset/PSET3/solutions/solution_1.py
@@ -0,0 +1,9 @@
+"""Write a python script that captures USN of a student from std. input and \
+converts all lowercase letters to uppercase and prints the same
+"""
+
+
+
+usn = input("please enter your USN\n")  # Enter the USN
+usn = usn.upper() # This will convet in to lower case to upper case
+print(usn)  #  print the uppercase converted USN.
