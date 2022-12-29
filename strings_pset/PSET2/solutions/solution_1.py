a3cdc6cc1e60d8f61ab475c0dba0a413b1a8716f
Omprakash <om381640@gmail.com>
2022-12-29 13:18:06 +0530

    Add all files

---
+++ strings_pset/PSET2/solutions/solution_1.py
@@ -0,0 +1,5 @@
+"""Write a python script that reads the name of the user and welcomes him. 
+"""
+
+name = input("please enter your name\n")  # enter the name
+print("Welcome",name)  #  Welcome with a name
