62e6690a0e34b34dbba20c6321a72d014fac4200
Omprakash <om381640@gmail.com>
2022-12-29 13:49:45 +0530

    Add files

---
+++ strings_pset/PSET1/solutions/solution_1.py
@@ -0,0 +1,5 @@
+"""Write a python script that reads an input from the std. input and prints the same in the std. output.
+"""
+
+var_1 = input()  # using input function it reads the std input
+print(var_1)  # print the same std output
