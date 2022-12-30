fce6632a659ac1f4b58b6bd68d727553609294ec
Omprakash <om381640@gmail.com>
2022-12-30 17:33:40 +0530

    Add files

---
+++ strings_pset/PSET10/solutions/solution_1.py
@@ -0,0 +1,12 @@
+"""Write and test a script that accepts user input using the input() function \
+and displays the result of trying to .find() a particular letter in that input.
+"""
+# Input is -->    work is worship
+
+
+s = input("please enter the i/p\n")  # Here enter the input in shell
+
+str_1 = s.find('i')  #  It will find the letter  lowest index of 'i'.
+
+print(str_1)  #  It will print the index 5
+
