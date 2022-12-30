ddb9e0bf568b64d1f8e42ce3bd937865c328310f
Omprakash <om381640@gmail.com>
2022-12-30 13:54:36 +0530

    Add files

---
+++ strings_pset/PSET8/solutions/solution_1.py
@@ -0,0 +1,20 @@
+"""In one line of code, display the result of .find() method for the following strings:
+"""
+
+var_1 = 'AAA'.find('a')  # In this string 'AAA' find using substring 'a' by using .find method
+print(var_1)  # If substring not found means output it wil return -1
+
+
+var_2 = 'aaa'.find('a')  #  In this string 'aaa' find substring 'a' by using .find method
+print(var_2) # If substring found means it will print the lowest index of 'a' is 0
+
+
+var_3 = 'adasdsfklshfshfd'.find('ds')  #  In this string 'adasdsfklshfshfd' find using substring 'ds' by using .find method
+print(var_3)  # If substring found means it will print lowest index of 'ds' is 4
+
+
+var_4 = 'adasdsfklshfshfd'.find('d')  #  In this string 'adasdsfklshfshfd' find using substring 'd' by using .find method
+print(var_4)  #  If substring found means it will print lowest index of 'd' is 1
+
+
+
