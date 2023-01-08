f6e12b2ac753bbdc87ae80889a60a92b7e5ec910
Omprakash <om381640@gmail.com>
2023-01-08 14:20:05 +0530

    Add all files

---
+++ strings_pset/PSET9/solutions/soilution_1.py
@@ -0,0 +1,6 @@
+"""Replace every occurrence of the character "a" with "b" 
+"""
+
+var_1 = input("Enter the string name\n")  # Enter the input for string name
+var_ch = var_1.replace("a","b")  #  by using replace method replace "a" with "b"
+print(var_ch) #  print the replaced characters
